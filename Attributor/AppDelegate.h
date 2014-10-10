bb48d81be92342a9bc54a6dcc42c3ca04b3a24b0
Lubman <lubman@USWPCOLUBMAN2.local>
2014-10-09 22:03:51 -0400

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Lubman, Steven on 10/9/14.
+//  Copyright (c) 2014 InfoPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Lubman, Steven on 10/9/14.
+//  Copyright (c) 2014 InfoPro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
