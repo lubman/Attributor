5392ccc5ac37b55e7887a4a6ee93c9bf0f0be889
Steven Lubman <lubman@USWPCOLUBMAN2.local>
2014-12-02 15:12:37 -0500

    Attributor
    Attributor sample project from Stanford iPhone course

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -10,9 +10,27 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
+
 @end
 
 @implementation ViewController
+- (IBAction)unoutlineBodySelection:(id)sender {
+}
+
+- (IBAction)outlineBodySelection:(UIButton *)sender {
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName : @-3,
+                                            NSStrokeColorAttributeName : [UIColor blackColor] }
+                                   range:self.body.selectedRange];
+}
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundofButton:(UIButton *)sender {
+    
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range: self.body.selectedRange];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
